582acb996c9b1f660428499b4c0f1971ef77ef7b
Dennis Hawran <gdatmo@gmail.com>
2022-04-08 15:33:11 +0200

    test

--- src/server.js
+++ src/server.js
@@ -6,18 +6,16 @@ const Url = require("url");
 const Mongo = require("mongodb");
 var Oasis;
 (function (Oasis) {
-    let port = process.env.PORT;
+    let port = process.env.PORT == undefined ? 5001 : process.env.PORT;
     let databaseURL = "mongodb+srv://Admin:OasisServer@cluster0.ayk2n.mongodb.net/Oasis?retryWrites=true&w=majority";
     let mongoClient = new Mongo.MongoClient(databaseURL);
     startServer(port);
     connectToDatabase(databaseURL);
     function startServer(_port) {
-        if (port == undefined)
-            port = 5001;
         let server = Http.createServer();
         server.listen(port);
-        console.log("listening on :" + port);
         server.addListener("request", handleRequest);
+        console.log("listening on :" + port);
     }
     async function connectToDatabase(_url) {
         await mongoClient.connect();
@@ -26,21 +24,20 @@ var Oasis;
     async function handleRequest(_request, _response) {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        console.log("Here");
         if (_request.url) {
             console.log(_request.url);
             let url = Url.parse(_request.url, true);
-            let newCommand = url.query["ghost"]?.toString();
-            if (newCommand == undefined)
-                newCommand = "";
+            let newCommand = url.query["ghost"] ? url.query["ghost"].toString() : "";
             let mongo = mongoClient.db("Oasis").collection("Commands");
             if (newCommand == "getCommand") {
                 _response.write("Command is: " + await mongo.find({ "ghost": { $exists: true } }));
             }
             else {
-                await mongo.updateOne({ "_id": "625025edc8b13bb0fd87915f" }, { $set: { "ghost": "HalloWorld" + newCommand.toString() } }, { upsert: true });
-                _response.write("Command received: " + newCommand);
+                let filter = { "_id": "625025edc8b13bb0fd87915f" };
+                let update = { "ghost": newCommand };
+                await mongo.updateOne(filter, update);
             }
+            _response.write("Command received: " + newCommand);
         }
         _response.end();
     }
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as Url from "url";
 import * as Mongo from "mongodb";
 
 export namespace Oasis {
-    let port: number | string | undefined = process.env.PORT;
+    let port: number | string = process.env.PORT == undefined ? 5001 : process.env.PORT;
     let databaseURL: string = "mongodb+srv://Admin:OasisServer@cluster0.ayk2n.mongodb.net/Oasis?retryWrites=true&w=majority";
     let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(databaseURL);
 
@@ -11,19 +11,16 @@ export namespace Oasis {
     connectToDatabase(databaseURL);
 
     function startServer(_port: number | string | undefined): void {
-        if (port == undefined)
-            port = 5001;
-
         let server: Http.Server = Http.createServer();
         server.listen(port);
-        console.log("listening on :" + port);
-
         server.addListener("request", handleRequest);
+
+        console.log("listening on :" + port);
     }
 
     async function connectToDatabase(_url: string): Promise<void> {
-
         await mongoClient.connect();
+
         console.log("Database connection is established");
     }
 
@@ -31,28 +28,25 @@ export namespace Oasis {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
 
-        console.log("Here");
         if (_request.url) {
-
             console.log(_request.url);
+
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
 
-            let newCommand: string = url.query["ghost"]?.toString()!;
-            if (newCommand == undefined)
-                newCommand = "";
+            let newCommand: string = url.query["ghost"] ? url.query["ghost"].toString() : "";
 
             let mongo: Mongo.Collection = mongoClient.db("Oasis").collection("Commands");
-
 
             if (newCommand == "getCommand") {
                 _response.write("Command is: " + await mongo.find({ "ghost": { $exists: true } }));
             } else {
-                await mongo.updateOne({ "_id": "625025edc8b13bb0fd87915f" }, { $set: { "ghost": "HalloWorld" + newCommand.toString() } }, { upsert: true });
-
-                _response.write("Command received: " + newCommand);
+                let filter: Mongo.Filter<any> = { "_id": "625025edc8b13bb0fd87915f" };
+                let update: Mongo.Document = { "ghost": newCommand };
+                await mongo.updateOne(filter, update);
             }
-        }
 
+            _response.write("Command received: " + newCommand);
+        }
         _response.end();
     }
 }
