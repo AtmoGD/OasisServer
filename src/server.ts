5516e72b2bf7e7f1c07651add8566b8da0a153cf
Dennis Hawran <gdatmo@gmail.com>
2022-04-08 16:11:44 +0200

    new

--- src/server.ts
+++ src/server.ts
@@ -41,7 +41,7 @@ export namespace Oasis {
                 _response.write("Command is: " + await mongo.find({ "ghost": { $exists: true } }));
             } else {
                 let filter: Mongo.Filter<any> = { _id: "625025edc8b13bb0fd87915f" };
-                let update: Mongo.Document = { ghost: 'Hello ${newCommand}' };
+                let update: Mongo.Document = { ghost: newCommand };
                 await mongo.updateOne(filter, {$set: update});
                 _response.write("Command received: " + newCommand);
             }
